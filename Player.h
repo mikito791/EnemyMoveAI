9eb2d841e120fc41c8f9d89dab1cba58e6eb9e91
幹人 永澤 <230413@jc-21.jp>
2025-03-05 13:29:16 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "./Library/GameObject.h"
+#include "./globals.h"
+
+class Player :
+    public GameObject
+{
+    int playerImage_;
+    Point pos_;
+public:
+    Player();
+    ~Player();
+    Point GetPos() { return pos_; }
+    void Update() override;
+    void Draw() override;
+    bool CheckHit(const Rect& me, const Rect& other);
+};
